54a480dc09b89bfa678d0daf480d551dc0510fe7
Alex <aagitolyev@kaaiot.io>
2018-11-06 21:15:53 +0200

    refactor: add type hints

--- emotiv_client/dto.py
+++ emotiv_client/dto.py
@@ -1,6 +1,6 @@
 class Request(object):
 
-    def __init__(self, method, params, id, jsonrpc="2.0"):
+    def __init__(self, method: str, params: dict, id: int, jsonrpc: str="2.0"):
         assert isinstance(params, dict)
         self.jsonrpc = jsonrpc
         self.method = method
