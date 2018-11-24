3567265b32469bd6ce1f70cb56d0a5f3c734262c
Alex <aagitolyev@kaaiot.io>
2018-11-24 16:58:47 +0200

    enhance: add auth parameters to config as env vars

--- emotiv_client/config.py
+++ emotiv_client/config.py
@@ -1,2 +1,12 @@
+import os
+
+
 class Config(object):
     URL = "wss://emotivcortex.com:54321"
+    USERNAME = os.getenv('EMOTIV_USERNAME', None)
+    PASSWORD = os.getenv('EMOTIV_PASSWORD', None)
+    CLIENT_ID = os.getenv('EMOTIV_CLIENT_ID', None)
+    CLIENT_SECRET = os.getenv('EMOTIV_CLIENT_SECRET', None)
+    # License Key
+    LICENSE = os.getenv('EMOTIV_LICENSE', None)
+    DEVICE_ID = os.getenv('EMOTIV_DEVICE_ID', None)
