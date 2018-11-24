d738840283dc6d8251f0191cb69a59f7ec04d1ba
Alex <aagitolyev@kaaiot.io>
2018-11-24 17:29:35 +0200

    enhance: add Response dto, factory method for Request

--- emotiv_client/dto.py
+++ emotiv_client/dto.py
@@ -1,8 +1,35 @@
+import sys
+import json
+import random
+
+
 class Request(object):
 
-    def __init__(self, method: str, params: dict, id: int, jsonrpc: str="2.0"):
-        assert isinstance(params, dict)
+    def __init__(self, method: str, id: int, jsonrpc: str="2.0", **kwargs):
+        self.id = id
         self.jsonrpc = jsonrpc
         self.method = method
-        self.params = params
+        self.params = kwargs
+
+    def to_string(self):
+        return json.dumps(self.__dict__)
+
+    @classmethod
+    def of(cls, method: str, params: dict):
+        return cls(method,  random.randint(0, sys.maxsize), **params)
+
+
+class Response(object):
+
+    def __init__(self, id: int, result=None, error=None, jsonrpc: str="2.0"):
         self.id = id
+        self.jsonrpc = jsonrpc
+        self.result = result
+        self.error = error
+
+    def to_string(self, pretty=False):
+        return json.dumps(self.__dict__) if not pretty else json.dumps(self.__dict__, indent=4)
+
+    @classmethod
+    def of_json(cls, resp: dict):
+        return cls(resp.get('id'), resp.get('result'), resp.get('error'), resp.get('jsonrpc'))
