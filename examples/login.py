16a8e0a4b12cc54dc72a46ebe8d7553a06668e41
Alex <aagitolyev@kaaiot.io>
2019-01-19 14:07:45 +0200

    fix: remove redundant parsing

--- examples/login.py
+++ examples/login.py
@@ -2,7 +2,6 @@ import asyncio
 import logging
 
 from emotiv_client.config import Config
-from emotiv_client.dto import Response
 from emotiv_client.cortex_context import CortexContext
 
 log = logging.getLogger(__name__)
@@ -17,5 +16,5 @@ if __name__ == '__main__':
                                                               Config.CLIENT_ID,
                                                               Config.CLIENT_SECRET))
 
-    login_resp = Response.of_json(asyncio.get_event_loop().run_until_complete(context.next()))
+    login_resp = asyncio.get_event_loop().run_until_complete(context.next())
     print(login_resp.to_string(pretty=True))
